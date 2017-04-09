c8521bdf8d6a8a98ce9d3f47c93ee638fca1cc01
Stephen Cline <stephenacline13@gmail.com>
2017-04-09 10:30:07 -0500

    Add specific college GET method

--- rest-server/server.py
+++ rest-server/server.py
@@ -17,23 +17,60 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import os
 import sqlite3
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify
 
 db_path = os.getcwd() + '/data/database/college-scorecard.sqlite'
 app = Flask(__name__)
 
-@app.route('/colleges', methods=['GET'])
+@app.route('/cscvis/api/v1.0/colleges', methods=['GET'])
 def get_colleges():
     """GET method for all colleges.
 
     Returns:
-        colleges: JSON list of all colleges, sorted by name.
+        colleges: JSON list of college names sorted alphabetically.
     """
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
     cur.execute('select instnm from College order by instnm')
     colleges = cur.fetchall()
+    for index in range(len(colleges)):
+        colleges[index] = colleges[index][0]
     return jsonify(colleges)
+
+@app.route('/cscvis/api/v1.0/colleges/<path:college_name>', methods=['GET'])
+def get_college(college_name):
+    """GET method for specific college. Returns data across all years.
+
+    Args:
+        college_name: College name that matches Scorecard's INSTNM property.
+
+    Returns:
+        college_data: JSON list of all data for the college formatted as a
+            dictionary: {"Table" : [value0, value1, value2...]}
+    """
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            'select college_id from College where INSTNM=?', (college_name,))
+        college_id = str(cur.fetchone()[0])
+    except:
+        abort(404)
+
+    cur.execute('select name from sqlite_master where type = "table"')
+    valid_tables = []
+    for table_name in cur.fetchall():
+        if table_name[0] in 'sqlite_sequence':
+            continue
+        valid_tables.append(table_name[0])
+
+    results = {}
+    for year_table in valid_tables:
+        cur.execute(
+            'select * from "%s" where college_id = ?' %
+                (year_table,), (college_id,))
+        results[year_table] = cur.fetchone()
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
